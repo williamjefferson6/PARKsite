ed69f8b8686b14a4515f1119a9f08a48d4e1897d
William Jefferson <2020600@iub.edu.bd>
2023-08-12 20:48:20 +0600

    Processing 1.0

--- login.js
+++ login.js
@@ -3,22 +3,31 @@ function load() {
     var urlToLoad = "";
     var number = Math.random();
 
-    if (selectedOption === "1" && number < 0.8) {
+    if (selectedOption === "1" && number < 0.7) {
         urlToLoad = "bookGarage.html";
     }
-    else if (selectedOption === "1" && number >= 0.8) {
+    else if (selectedOption === "1" && number >= 0.7 && number < 0.85) {
+        urlToLoad = "processing.html";
+    }
+    else if (selectedOption === "1" && number >= 0.85) {
         urlToLoad = "ban.html";
     } 
-    else if (selectedOption === "2" && number < 0.8) {
+    else if (selectedOption === "2" && number < 0.7) {
         urlToLoad = "offerGarage.html";
-    } 
-    else if (selectedOption === "2" && number >= 0.8) {
+    }
+    else if (selectedOption === "2" && number >= 0.7 && number < 0.85) {
+        urlToLoad = "processing.html";
+    }
+    else if (selectedOption === "2" && number >= 0.85) {
         urlToLoad = "ban.html";
     } 
-    else if (selectedOption === "3" && number < 0.8) {
+    else if (selectedOption === "3" && number < 0.7) {
         urlToLoad = "supervise.html";
     }
-    else if (selectedOption === "3" && number >= 0.8) {
+    else if (selectedOption === "3" && number >= 0.7 && number < 0.85) {
+        urlToLoad = "processing.html";
+    }
+    else if (selectedOption === "3" && number >= 0.85) {
         urlToLoad = "ban.html";
     } 
 
